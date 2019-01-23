3b42f657a1d782734b5aeedac4243bfdf04b73f3
Synic1 <12225911+Synic1@users.noreply.github.com>
2019-01-22 22:34:34 -0600

    added axios and base request model and a example request

---
+++ src/api/App.v1.js
@@ -0,0 +1,15 @@
+import Request from './request';
+
+class App extends Request {
+  constructor() {
+    super('todos');
+  }
+
+  get(n = '') {
+    return this._get('\\' + n, null, { userId: 2, completed: true });
+  }
+}
+
+const app = new App();
+
+export default app;
